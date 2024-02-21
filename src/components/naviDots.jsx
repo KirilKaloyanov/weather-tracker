f19d17f3352cc114e93b436aa848f034225ec042
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2024-02-21 09:57:52 +0200

    Adds NaviArrows

--- src/components/naviDots.jsx
+++ src/components/naviDots.jsx
@@ -1,20 +1,32 @@
+import { last } from "lodash";
+
 function NaviDots({ navigationDotsCount, scrollToIndex, scrollLeftValue }) {
-  const spanArray = Array.from({ length: navigationDotsCount });
-  return (
+  let currentIndex = scrollLeftValue / 350;
+  const zeroIndex = currentIndex == 0;
+  const lastIndex = currentIndex == navigationDotsCount - 1;
+  console.log(lastIndex);
+  const dotsArray = Array.from({ length: navigationDotsCount });
+  return navigationDotsCount > 1 ? (
     <div className="navigation_dots">
-      {navigationDotsCount > 1
-        ? spanArray.map((sp, index) => (
-            <div
-              key={index}
-              className={`border ${
-                scrollLeftValue == index * 350 ? "filled_dot" : "empty_dot"
-              }`}
-              onClick={() => scrollToIndex(index)}
-            ></div>
-          ))
-        : null}
+      <button
+        className="arrow left"
+        onClick={!zeroIndex ? () => scrollToIndex(--currentIndex) : null}
+      ></button>
+      {dotsArray.map((sp, index) => (
+        <div
+          key={index}
+          className={`dot border ${
+            currentIndex == index ? "filled_dot" : "empty_dot"
+          }`}
+          onClick={() => scrollToIndex(index)}
+        ></div>
+      ))}
+      <button
+        className="arrow right"
+        onClick={!lastIndex ? () => scrollToIndex(++currentIndex) : null}
+      ></button>
     </div>
-  );
+  ) : null;
 }
 
 export default NaviDots;
